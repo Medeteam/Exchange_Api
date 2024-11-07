fdf18c5c90ddd2b8dfb218393bc1e243b65451a6
sebasarias17 <ariasusmasebastian@gmail.com>
2024-11-07 10:19:26 -0500

    Market Created

--- exchange_api/api/urls.py
+++ exchange_api/api/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path, include
-from .views import CryptoListCreateView, CryptoDetailView, StockListCreateView, UserListView, SignupView, LoginView, cryptoHistoryView, cryptoHistoryIdView, cryptoHistorySymbolView, StockDetailView, stockHistorySymbolView, stockHistoryIdView, stockHistoryView
+from .views import CryptoListCreateView, CryptoDetailView, StockListCreateView, UserListView, SignupView, LoginView, cryptoHistoryView, cryptoHistoryIdView, cryptoHistorySymbolView, StockDetailView, stockHistorySymbolView, stockHistoryIdView, stockHistoryView, MarketView
 from rest_framework.routers import DefaultRouter
 
 # router = DefaultRouter()
 # router.register(r'api/users', UserListView)
 
 urlpatterns = [
-    path('api/crypto/', CryptoListCreateView.as_view(), name='crypto-list-create'),
-    path('api/crypto/<str:symbol>/', CryptoDetailView.as_view(), name='crypto-detail'),
-    path('api/stock/', StockListCreateView.as_view(), name='stock-list-create'),
-    path('api/stock/<str:symbol>/', StockDetailView.as_view(), name='stock-detail'),
-    # path('', include(router.urls)),
+    path ('api/market', MarketView.as_view(), name = 'market-list'),
+    path('api/market/crypto/', CryptoListCreateView.as_view(), name='crypto-list-create'),
+    path('api/market/crypto/<str:symbol>/', CryptoDetailView.as_view(), name='crypto-detail'),
+    path('api/market/stock/', StockListCreateView.as_view(), name='stock-list-create'),
+    path('api/market/stock/<str:symbol>/', StockDetailView.as_view(), name='stock-detail'),
     path('api/users/', UserListView.as_view(), name='user-list'),
     path('api/auth/signup/', SignupView.as_view(), name='signup'),
     path('api/auth/login/', LoginView.as_view(), name='login'),
